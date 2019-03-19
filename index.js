f0698766b92d8465ecdd5ef3ad9bb5ede2792c36
jamiebdavis <jamied1985@gmail.com>
2019-03-19 12:41:19 +0000

    completed basic imput problem, need to refactor, add tests and write documentation

--- index.js
+++ index.js
@@ -1,37 +1,93 @@
 //Storing variabels at global level
-let coordinatesR = 0;
-let coordinatesL = 0;
+let coordinatesX = 0;
+let coordinatesY = 0;
+let direction = "N";
 
-function droneMovement(input) {
+//function to work out which direction drone is facing
+function droneDirection(input) {
   //splitting to input in to array
   let directions = input.split("");
 
-  //depending on first character in array, will iterate over each value and update in correct coordinates
-
-  if (directions[0] === "R") {
-    directions.map(char => {
-      if (char === "+") {
-        return (coordinatesR += 1);
-      }
-      if (char === "-") {
-        return (coordinatesR -= 1);
-      }
-    });
+  //Might convert into a switch statement
+  //Clockwise direction
+  if (direction === "N" && directions[0] === "R") {
+    return (direction = "E");
   }
-
-  if (directions[0] === "L") {
-    directions.map(char => {
-      if (char === "+") {
-        return (coordinatesL += 1);
-      }
-      if (char === "-") {
-        return (coordinatesL -= 1);
-      }
-    });
+  if (direction === "E" && directions[0] === "R") {
+    return (direction = "S");
   }
-
-  //returning value as string
-  return `${String(coordinatesR)},${String(coordinatesL)}`;
+  if (direction === "S" && directions[0] === "R") {
+    return (direction = "W");
+  }
+  if (direction === "W" && directions[0] === "R") {
+    return (direction = "N");
+  }
+  //Anticlockwise direction
+  if (direction === "N" && directions[0] === "L") {
+    return (direction = "W");
+  }
+  if (direction === "W" && directions[0] === "L") {
+    return (direction = "S");
+  }
+  if (direction === "S" && directions[0] === "L") {
+    return (direction = "E");
+  }
+  if (direction === "E" && directions[0] === "L") {
+    return (direction = "N");
+  }
+  //might but else clause in here for anything which doesnt contain R or L
 }
 
-droneMovement("L--");
+function droneInput(input) {
+  //importing droneDirection function to determine what direction the drone is facing
+  droneDirection(input);
+
+  //splitting to input in to array
+  let directions = input.split("");
+
+  if (direction === "N") {
+    //iterate over array to extract "-" or "+" to determine wether done goes forward of backwards
+    directions.map(char => {
+      if (char === "+") {
+        return (coordinatesY += 1);
+      }
+      if (char === "-") {
+        return (coordinatesY -= 1);
+      }
+    });
+  }
+  if (direction === "E") {
+    directions.map(char => {
+      if (char === "+") {
+        return (coordinatesX += 1);
+      }
+      if (char === "-") {
+        return (coordinatesX -= 1);
+      }
+    });
+  }
+  if (direction === "S") {
+    directions.map(char => {
+      if (char === "+") {
+        return (coordinatesY -= 1);
+      }
+      if (char === "-") {
+        return (coordinatesY += 1);
+      }
+    });
+  }
+  if (direction === "W") {
+    directions.map(char => {
+      if (char === "+") {
+        return (coordinatesX -= 1);
+      }
+      if (char === "-") {
+        return (coordinatesX += 1);
+      }
+    });
+  }
+  console.log("Drone direction: " + direction);
+
+  //returning value as string as it appears on scenario.pdf
+  return `${String(coordinatesX)},${String(coordinatesY)}`;
+}
